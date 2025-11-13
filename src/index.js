1321ff4ff5ee044311d8bccb7c916886af71d594
Twi <wyfsix@gmail.com>
2025-11-13 08:48:23 -0500

    Plain CSS weapons sheet app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';       // Tailwind styling
+import './index.css';  // <-- your new plain CSS
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
